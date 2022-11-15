5bb7ea0ebfb92bed091bfefad4e845253ad00211
riverami <71803404+riverami@users.noreply.github.com>
2022-11-14 18:09:45 -0600

    Add files via upload
    Revision 1

--- microService.py
+++ microService.py
@@ -5,6 +5,10 @@
 # https://docs.python.org/3/library/sqlite3.html
 # https://docs.python.org/3/library/socket.html
 
+#Table:
+#date, water temp (F), air temp(F), time of the first peak high tide
+#weather(date, water_temp, air_temp, first_peak_time)
+
 import sys
 from os.path import exists
 import sqlite3
@@ -12,13 +16,31 @@ import time
 import socket
 
 
-def getData(data1):
-    returnData = cur.execute("SELECT * FROM data WHERE title = '" + data1 + "'")
+def getDataBetweenDate(lowDate, highDate):
+    returnData = cur.execute("SELECT * FROM weather WHERE date BETWEEN '" + lowDate + "' AND '" + highDate + "'")
     return returnData
 
 
-def insertData(data1, data2, data3):
-    cur.execute("INSERT INTO data VALUES (" + "'" + str(data1) + "'" + ", " + str(data2) + ", " + str(data3) + ")")
+def getDataBetweenWaterTemp(lowTemp, highTemp):
+    #lowTemp = int(lowTemp)
+    #highTemp = int(highTemp)
+    returnData = cur.execute("SELECT * FROM weather WHERE water_temp BETWEEN " + lowTemp + " AND " + highTemp)
+    return returnData
+
+
+def getDataBetweenAirTemp(lowTemp, highTemp):
+    returnData = cur.execute("SELECT * FROM weather WHERE air_temp BETWEEN " + lowTemp + " AND " + highTemp)
+    return returnData
+
+
+def getDataBetweenPeakTime(lowTime, highTime):
+    returnData = cur.execute("SELECT * FROM weather WHERE first_peak_time BETWEEN '" + lowTime + "' AND '" + highTime + "'")
+    return returnData
+
+
+def insertData(newDate, newWaterTemp, newAirTemp, newTime):
+    cur.execute("INSERT INTO weather VALUES (" + "'" + str(newDate) + "'" + ", " + str(newWaterTemp)
+                + ", " + str(newAirTemp) + ", " + str(newTime) + ")")
     conn.commit()
 
 
@@ -36,8 +58,9 @@ else:
     f = open("data.db", "x")
     conn = sqlite3.connect("data.db")
     cur = conn.cursor()
-    cur.execute("CREATE TABLE data(title, year, score)")
-    cur.execute("INSERT INTO data VALUES ('Movie1', 1990, 9.1), ('Movie2', 1952, 7.4), ('Movie3', 1984, 6.2)")
+    cur.execute("CREATE TABLE weather(date, water_temp, air_temp, first_peak_time)")
+    cur.execute("INSERT INTO weather VALUES ('07-31-2022', 80, 95, '16:00'),"
+                " ('08-01-2022', 95, 80, '17:00'), ('06-15-2022', 70, 80, '12:00')")
     conn.commit()
 
 
@@ -60,20 +83,44 @@ while True:
     words = message.split(',')
     message = ""
     retData = ""
+    print(words[0])
+    print(words[1])
+    print(words[2])
 
-    if words[0] == "GET" and words[1] != "" and words[1] is not None:
-        sqlData = getData(str(words[1].strip()))
+    if words[0] == "GETONDATE" and words[1] != "" and words[1] is not None and\
+            words[2].strip() != "" and words[2].strip() is not None:
+        sqlData = getDataBetweenDate(str(words[1].strip()), str(words[2].strip()))
         listData = sqlData.fetchall()
         retData = str(listData)
+
+    elif words[0] == "GETONWATERTEMP" and words[1] != "" and words[1] is not None and\
+            words[2].strip() != "" and words[2].strip() is not None:
+        sqlData = getDataBetweenWaterTemp(words[1].strip(), words[2].strip())
+        listData = sqlData.fetchall()
+        retData = str(listData)
+
+    elif words[0] == "GETONAIRTEMP" and words[1] != "" and words[1] is not None and\
+            words[2].strip() != "" and words[2].strip() is not None:
+        sqlData = getDataBetweenAirTemp(str(words[1].strip()), str(words[2].strip()))
+        listData = sqlData.fetchall()
+        retData = str(listData)
+
+    elif words[0] == "GETONPEAKTIME" and words[1] != "" and words[1] is not None and \
+            words[2].strip() != "" and words[2].strip() is not None:
+        sqlData = getDataBetweenPeakTime(str(words[1].strip()), str(words[2].strip()))
+        listData = sqlData.fetchall()
+        retData = str(listData)
+
     elif words[0].strip() == "POST" and words[1].strip() != "" and words[1].strip() is not None and \
             words[2].strip() != "" and words[2].strip() is not None and words[3].strip() != "" \
-            and words[3].strip() is not None:
+            and words[3].strip() is not None and words[4].strip() != "" and words[4].strip() is not None:
 
-        data1 = words[1].strip()
-        data2 = words[2].strip()
-        data3 = words[3].strip()
+        newDate = words[1].strip()
+        newWaterTemp = words[2].strip()
+        newAirTemp = words[3].strip()
+        newTime = words[4].strip()
 
-        insertData(data1, data2, data3)
+        insertData(newDate, newWaterTemp, newAirTemp, newTime)
         retData = "Data has been entered"
     else:
         retData = "Please enter a valid command with valid arguments"
